6caa3b7bef301b71f41d195e67c42dbdd5023ea6
ahmadsufyan455 <ahmadsufyan455@gmail.com>
2022-06-10 22:53:13 +0700

    dashboard: add bottom navigation bar

--- lib/modules/dashboard/dashboard.dart
+++ lib/modules/dashboard/dashboard.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:frent_jogja/modules/dashboard/booking/booking.dart';
+import 'package:frent_jogja/modules/dashboard/home/home.dart';
+import 'package:frent_jogja/modules/dashboard/profile/profile.dart';
+import 'package:frent_jogja/utils/styles.dart';
 
 class Dashboard extends StatefulWidget {
   const Dashboard({Key? key}) : super(key: key);
@@ -10,8 +14,44 @@ class Dashboard extends StatefulWidget {
 }
 
 class _DashboardState extends State<Dashboard> {
+  int _currentIndex = 0;
+  final List<Widget> _children = const [
+    HomeScreen(),
+    BookingScreen(),
+    ProfileScreen(),
+  ];
+
+  void onTabTapped(int index) {
+    setState(() {
+      _currentIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: _children[_currentIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        onTap: onTabTapped,
+        currentIndex: _currentIndex,
+        selectedItemColor: kRed,
+        unselectedItemColor: kBlack,
+        type: BottomNavigationBarType.fixed,
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home_filled),
+            label: 'Beranda',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.receipt),
+            label: 'Pemesanan',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.account_circle),
+            label: 'Akun',
+          ),
+        ],
+      ),
+    );
   }
 }
